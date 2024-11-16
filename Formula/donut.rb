20615855228db0c9d9a61726c7ea22ad912ef85f
aa830 <84448225+aa830@users.noreply.github.com>
2024-11-16 21:19:45 +0800

    Oops! Forgot to change the version!

--- Formula/donut.rb
+++ Formula/donut.rb
@@ -3,7 +3,7 @@ class Donut < Formula
   homepage "https://github.com/aa830/donut.c"
   url "https://github.com/aa830/donut.c/archive/refs/tags/0.1b.tar.gz"
   sha256 "d42075580760c0d2eb663d8f4cefaecd5438a046638e2d0c48037e1caeb7dfea"
-  version "0.1a"
+  version "0.1b"
 
   def install
     bin.install "donut"  # This line must match the binary you want to install
