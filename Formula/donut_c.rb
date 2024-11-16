29d755c2d0d80616b943e3f590d8a1436d5b55e1
aa830 <84448225+aa830@users.noreply.github.com>
2024-11-16 20:55:05 +0800

    Update SHA256 checksum in donut_c.rb formula

--- Formula/donut_c.rb
+++ Formula/donut_c.rb
@@ -2,7 +2,7 @@ class DonutC < Formula
   desc "A spinning donut in the terminal"
   homepage "https://github.com/aa830/donut.c"
   url "https://github.com/aa830/donut.c/archive/refs/tags/0.1a.tar.gz"
-  sha256 "423d5b3822c0bd38ccd3c4ebd886855d14e6cfb7c38c7eef9019b2ded832ff69"
+  sha256 "1541accb844e65eb011f26855cd30b637b377467be06d683d7fe5e5fd94df842"
   license "MIT"
 
   def install
