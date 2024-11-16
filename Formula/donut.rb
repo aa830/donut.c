ad22482be478cbc7a0a704d6c0cbe95caab459bc
aa830 <84448225+aa830@users.noreply.github.com>
2024-11-16 21:20:57 +0800

    Ugh sha256 T-T

--- Formula/donut.rb
+++ Formula/donut.rb
@@ -2,7 +2,7 @@ class Donut < Formula
   desc "The original donut.c, but better!"
   homepage "https://github.com/aa830/donut.c"
   url "https://github.com/aa830/donut.c/archive/refs/tags/0.1b.tar.gz"
-  sha256 "d42075580760c0d2eb663d8f4cefaecd5438a046638e2d0c48037e1caeb7dfea"
+  sha256 "61feb3a2677c1802d09459fd3bcdb977657a7af4159717b24285e0d70c5f34d1"
   version "0.1b"
 
   def install
