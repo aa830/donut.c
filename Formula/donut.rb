d26951896ddd1930fb8419271325d56b1a1447bf
aa830 <84448225+aa830@users.noreply.github.com>
2024-11-17 09:38:51 +0800

    Fixed formula

--- Formula/donut.rb
+++ Formula/donut.rb
@@ -1,9 +1,9 @@
 class Donut < Formula
   desc "The original donut.c, but better!"
   homepage "https://github.com/aa830/donut.c"
-  url "https://github.com/aa830/homebrew-donut.c/archive/refs/tags/0.1e.tar.gz"
-  sha256 "a9e15a45d2a38932ad6be7d72b26318213e76bc129caef739552dd00ff95f833"
-  version "0.1e"
+  url "https://github.com/aa830/homebrew-donut.c/archive/refs/tags/v0.1-pre.tar.gz"
+  sha256 "37486df847b53709cfc94522b15102b2ddecdf472b4f343c4244f9d3308b4b6a"
+  version "v0.1-pre"
 
   def install
     # Compile the project using `make`
