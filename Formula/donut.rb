ea00a20c924267d40452791f150c0360115e58fc
aa830 <84448225+aa830@users.noreply.github.com>
2024-11-17 09:16:57 +0800

    Updated version and release for homebrew

--- Formula/donut.rb
+++ Formula/donut.rb
@@ -1,9 +1,9 @@
 class Donut < Formula
   desc "The original donut.c, but better!"
   homepage "https://github.com/aa830/donut.c"
-  url "https://github.com/aa830/donut.c/archive/refs/tags/0.1b.tar.gz"
-  sha256 "61feb3a2677c1802d09459fd3bcdb977657a7af4159717b24285e0d70c5f34d1"
-  version "0.1b"
+  url "https://github.com/aa830/homebrew-donut.c/archive/refs/tags/0.1c.tar.gz"
+  sha256 "3ca18d436df36b12ce223310a29808f5b5a7ee839a22cb943801eddb46d8613f"
+  version "0.1c"
 
   def install
     bin.install "donut"  # This line must match the binary you want to install
