df020dc952f0335b9d6eac20c655611445690a27
aa830 <84448225+aa830@users.noreply.github.com>
2024-11-17 09:50:11 +0800

    Fixed the tarball name for url

--- Formula/donut.rb
+++ Formula/donut.rb
@@ -1,7 +1,7 @@
 class Donut < Formula
   desc "The original donut.c, but better!"
   homepage "https://github.com/aa830/donut.c"
-  url "https://github.com/aa830/homebrew-donut.c/archive/refs/tags/v0.1.2.tar.gz"
+  url "https://github.com/aa830/homebrew-donut.c/archive/refs/tags/0.1.2.tar.gz"
   sha256 "a7fa16933e5a10bcb07c205af38f415db113619f3f95cd7f89accb646bea5848"
   version "0.1.2"
 
