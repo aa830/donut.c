5ca17bb9826020a84bd1d336c10ce0b45dd48a46
aa830 <84448225+aa830@users.noreply.github.com>
2024-11-20 21:35:26 +0800

    new feature!!!

--- Formula/donut.rb
+++ Formula/donut.rb
@@ -1,9 +1,9 @@
 class Donut < Formula
   desc "The original donut.c, but better!"
   homepage "https://github.com/aa830/donut.c"
-  url "https://github.com/aa830/homebrew-donut.c/archive/refs/tags/0.1.6.tar.gz"
+  url "https://github.com/aa830/homebrew-donut.c/archive/refs/tags/0.1.7.tar.gz"
   sha256 "18284816a6c955c9720a2db6db715e00c6b730aa5201e71d3ac3bf0e22d8a5ab"
-  version "0.1.6"
+  version "0.1.7"
 
   def install
     # Compile the project using `make`
